2617b6ca0b8478da1ecc696a9f1e2012fec23320
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-11 23:21:23 +0530

    Add username page and update dashboard page

---
+++ app/[username]/page.tsx
@@ -0,0 +1,4 @@
+export default function Page({ params }: { params: { username: string } }) {
+
+  return <div>My Post: {params.username}</div>;
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,75 @@
 import LinkCard from '@/components/links/link';
 
 export default function Page() {
+  const links = [
+    {
+      id: 1,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 2,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 3,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 4,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 5,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 6,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 7,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 8,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 9,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 10,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 11,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+    {
+      id: 12,
+      title: '10 resources to get the first job',
+      url: 'https://www.google.com',
+    },
+  ];
+
   return (
     <section className="max-w-7xl mx-auto">
       <div className="py-10 flex flex-col gap-y-4 sm:w-1/2 w-full">
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
+        {links.map((link) => (
+          <LinkCard key={link.id} link={link} />
+        ))}
       </div>
     </section>
   );

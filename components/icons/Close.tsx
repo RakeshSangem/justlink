ccb7bc70750c82d7e672cceb1f131b15436148c6
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-11 23:20:49 +0530

    Add Close icon component

---
+++ components/icons/Close.tsx
@@ -0,0 +1,16 @@
+export default function Close() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      fill="none"
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+    >
+      <path d="M18 6L6 18M6 6l12 12"></path>
+    </svg>
+  );
+}

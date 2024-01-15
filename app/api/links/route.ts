e93c5d5d4382548586477c7d31b8dcb4f745cd38
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-15 23:04:39 +0530

    Add API routes for creating, getting, and updating links

---
+++ app/api/links/route.ts
@@ -0,0 +1,71 @@
+// pages/api/link.js
+import connect from '@/db';
+import Link from '@/models/link.model';
+
+export async function POST(req: Request, res: Response) {
+  if (req.method === 'POST') {
+    try {
+      await connect();
+      const body = await req.json();
+
+      const newLink = new Link({
+        title: body.title,
+        url: body.url,
+      });
+
+      const savedLink = await newLink.save();
+
+      return Response.json(savedLink);
+
+      // // Validate and create a new link
+      // const newLink = new Link({
+      //   title,
+      //   url,
+      //   // other fields
+      // });
+
+      // const savedLink = await newLink.save();
+
+      // res.status(201).json(savedLink);
+    } catch (error) {
+      console.error('Error creating link:', error);
+      // res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    // res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
+
+export async function GET(req: Request, res: Response) {
+  if (req.method === 'GET') {
+    try {
+      await connect();
+      const links = await Link.find({});
+      return Response.json(links);
+    } catch (error) {
+      console.error('Error getting links:', error);
+      // res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    // res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
+
+export async function PUT(req: Request, res: Response) {
+  if (req.method === 'PUT') {
+    
+    try {
+      await connect();
+      const body = await req.json();
+      const updatedLink = await Link.findByIdAndUpdate(body._id, body, {
+        new: true,
+      });
+      return Response.json(updatedLink);
+    } catch (error) {
+      console.error('Error updating link:', error);
+      // res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    // res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}

594f1d5da8296e50790f73e0aa5b33a5dffa5fa3
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-18 15:32:24 +0530

    Remove unused Modal component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,10 @@
 import { ReactNode } from 'react';
 import NavTabs from '@/components/nav-tabs';
 import Providers from './providers';
-import Modal from '@/components/modals/Modal';
-import ModalProvider from '@/components/modals/ModalProvider';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <Providers>
-      {/* <ModalProvider> */}
       <section className="min-h-screen max-w-screen-xl mx-auto">
         <nav className="flex justify-between items-center w-full px-8 py-4 border-b border-b-gray-800">
           <span className="text-2xl font-bold">JustLink</span>
@@ -24,7 +21,6 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
           {children}
         </div>
       </section>
-      {/* </ModalProvider> */}
     </Providers>
   );
 }

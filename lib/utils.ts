4cb9f5d26d30afb684566e0c382a26671415712e
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-19 13:59:10 +0530

    Add URL validation function to utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+// also check the url with regex
+export function isUrlValid(url: string) {
+  try {
+    const newUrl = new URL(url);
+
+    if (newUrl.protocol !== 'http:' && newUrl.protocol !== 'https:') {
+      return false;
+    }
+
+    const urlRegex =
+      /^(https?:\/\/)?([\w-]+(\.[\w-]+)+\/?)([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/;
+
+    return urlRegex.test(url);
+  } catch (_) {
+    return false;
+  }
+
+  return true;
+}

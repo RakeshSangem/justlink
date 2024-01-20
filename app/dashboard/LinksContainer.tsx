0c716fb94fc62bb75ff759e66f619135cd13f9ce
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-20 22:49:13 +0530

    Refactor LinksContainer component to include a LinkCardSkeleton when isValidating

--- app/dashboard/LinksContainer.tsx
+++ app/dashboard/LinksContainer.tsx
@@ -1,15 +1,30 @@
 'use client';
 
+import { Key } from 'react';
 import LinkCard from '@/components/links/LinkCard';
 import useLinks from '@/lib/swr/use-links';
-import { Key } from 'react';
 
 export default function LinksContainer() {
   const { links, isValidating } = useLinks();
 
   return (
     <div className="flex flex-col gap-y-3">
-      {links?.map((link: any, idx: Key) => <LinkCard key={idx} link={link} />)}
+      {isValidating
+        ? [...Array(5)].map((_, idx) => <LinkCardSkeleton key={idx} />)
+        : links?.map((link: any, idx: Key) => (
+            <LinkCard key={idx} link={link} />
+          ))}
+    </div>
+  );
+}
+
+function LinkCardSkeleton() {
+  return (
+    <div className="isolate flex w-full items-start rounded-md bg-[#D9D9D9]/15 px-6 py-5 duration-300">
+      <div className="flex flex-1 flex-col gap-y-2 ">
+        <div className="h-5 w-4/5 rounded-sm bg-[#D9D9D9]/5"></div>
+        <div className="h-3 w-1/3 rounded-sm bg-[#D9D9D9]/5"></div>
+      </div>
     </div>
   );
 }

ec8bdc2ddca5c292cd91015b217d51ea88108b72
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-29 21:17:45 +0530

    Refactor currentUser function to return a typed Promise<User | null>

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,11 +1,12 @@
 import { auth } from '@/auth';
+import type { Session, User } from 'next-auth';
 
-export const currentUser = async () => {
+export const currentUser = async (): Promise<User | null> => {
   const session = await auth();
 
   if (!session) {
     return null;
   }
 
-  return session.user;
+  return session?.user as User;
 };

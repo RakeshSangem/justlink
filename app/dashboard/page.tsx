757f3ba1d46ed1947b5697288c4884d0becaf1f0
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-29 21:17:12 +0530

    Add authentication check and redirect to login page if session is not available

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,25 @@
 import PageClient from './page-client';
 import LinksContainer from './LinksContainer';
 import { MaxWidthWrapper } from '@/components/MaxWidthWrapper';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (!session) {
+    redirect('/login');
+    return null;
+  }
+
   return (
-    <section className="max-w-7xl mx-auto px-2 sm:px-8">
-      <div className="py-10 flex flex-col gap-y-4 sm:w-1/2 w-full">
+    <section className="relative max-w-7xl mx-auto px-2 sm:px-8 flex">
+      <div className="py-10 flex flex-col gap-y-4 w-full lg:w-3/5">
         <MaxWidthWrapper>
           <PageClient />
           <LinksContainer />
         </MaxWidthWrapper>
       </div>
+      <div className="mx-auto relative"></div>
     </section>
   );
 }

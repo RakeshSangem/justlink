ec5e4ed1dce747bae9952727859f74c29b624c9c
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-01-31 23:31:54 +0530

    Remove unused code for Google profile mapping

--- auth.config.ts
+++ auth.config.ts
@@ -11,15 +11,6 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      profile(profile) {
-        console.log('profile', profile);
-        return {
-          id: profile.id,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
     }),
   ],
 } satisfies NextAuthConfig;

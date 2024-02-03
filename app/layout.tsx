79af925d15171e841fc8fcebae532f251cf6b6fb
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-02-03 20:10:15 +0530

    Add Providers component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Toaster } from 'sonner';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Providers from './dashboard/providers';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <Toaster />
+        <Providers>
+          {children}
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );

a03fd9787df7f20ddaaa640091a41d9d81e09aa2
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-02-03 20:11:35 +0530

    Refactor LinksContainer component

--- app/dashboard/LinksContainer.tsx
+++ app/dashboard/LinksContainer.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Key } from 'react';
 import LinkCard from '@/components/links/LinkCard';
 import useLinks from '@/lib/swr/use-links';
 
@@ -9,18 +8,17 @@ export default function LinksContainer() {
 
   return (
     <div className="flex flex-col gap-y-3">
-      {isValidating
-        ? [...Array(5)].map((_, idx) => <LinkCardSkeleton key={idx} />)
-        : links?.map((link: any, idx: Key) => (
-            <LinkCard key={idx} link={link} />
-          ))}
+      {isValidating || !links
+        ? [...Array(3)].map((_, idx) => <LinkCardSkeleton key={idx} />)
+        : links?.map((link: any) => <LinkCard key={link.id} link={link} />)}
     </div>
   );
 }
 
 function LinkCardSkeleton() {
   return (
-    <div className="isolate flex w-full items-start rounded-md bg-[#D9D9D9]/15 px-6 py-5 duration-300">
+    <div className="relative overflow-hidden isolate flex w-full items-start rounded-md bg-[#D9D9D9]/15 px-6 py-5 duration-300">
+      <div className="absolute animate-shimmer left-0 top-0 h-full w-full -translate-x-full bg-gradient-to-r from-transparent via-gray-200/10 to-transparent"></div>
       <div className="flex flex-1 flex-col gap-y-2 ">
         <div className="h-5 w-4/5 rounded-sm bg-[#D9D9D9]/5"></div>
         <div className="h-3 w-1/3 rounded-sm bg-[#D9D9D9]/5"></div>

f1270cae9a06eca6966a676e917f1ff93583b024
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-02-08 22:18:59 +0530

    Refactor user page component

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,41 +1,61 @@
-export default async function Page({
-  params,
-}: {
+import NotFound from '../not-found';
+
+interface PageProps {
   params: { username: string };
-}) {
-  const res = await fetch(`http://localhost:3000/api/links`);
-  const links = await res.json();
+}
+
+export const dynamic = 'force-dynamic';
+
+export default async function Page({ params }: PageProps) {
+  console.log('params from page', params.username);
+
+  const res = await fetch(`http://localhost:3000/api/user/rakeshsangem`, {
+    cache: 'no-store',
+  });
+
+  console.log('res from page', res);
+
+  const user = await res.json();
+
+  console.log('user from page', user?.user?.links);
+
+  // const { name, links, avatar } = user;
+
+  // console.log('user from page', user);
+
+  if (!user) {
+    return <NotFound />;
+  }
 
   return (
-    <div className="p-3 sm:p-10 max-w-2xl mx-auto min-h-screen">
+    <div className="p-3 mt-6 sm:p-10 max-w-2xl mx-auto min-h-screen">
       <div className="w-20 h-20 bg-red-300 rounded-full mx-auto my-2">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://avatars.githubusercontent.com/u/19484515?v=4"
+          src={user?.user?.avatar}
           alt="avatar"
           className="w-20 h-20 bg-red-300 rounded-full mx-auto my-2"
         />
       </div>
 
-      <h1 className="text-3xl font-bold mb-4 text-center">{params.username}</h1>
-      <p className="text-center">
-        Fontend developer, I love to build things with React, Next.js and
-        Tailwind CSS 🚀
-      </p>
-
-      {links &&
-        links.map((link: any) => {
-          return (
+      <h1 className="text-3xl font-bold mb-4 text-center">
+        {user?.user?.name}
+      </h1>
+      <p className="text-center">{user?.user?.bio}</p>
+      {user?.user?.links?.length > 0 && (
+        <>
+          {user?.user?.links.map((link: any) => (
             <a
               href={link.url}
-              target="_blank"
-              className="bg-white block rounded-md text-center my-4 text-black px-10 sm:py-4 py-2"
-              key={link._id}
+              target="__blank"
+              className="bg-white block rounded-md text-center my-4 text-black px-10 sm:py-2.5 py-2"
+              key={link.id}
             >
               {link.title}
             </a>
-          );
-        })}
+          ))}
+        </>
+      )}
       <br />
       <footer className="py-4 w-full flex justify-center">
         <button className="px-6 py-2 bg-white/80 rounded-md text-black mx-auto">

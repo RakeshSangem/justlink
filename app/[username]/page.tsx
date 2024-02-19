574cb741736f40fa9ae75c9c03e4daa4d212882e
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-02-19 22:40:15 +0530

    Refactor user page layout and styles

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,71 +1,66 @@
 import Logo from '@/components/Logo';
 import NotFound from '../not-found';
+import { title } from 'process';
 
-interface PageProps {
+interface Props {
   params: { username: string };
 }
 
 export const dynamic = 'force-dynamic';
 
-export default async function Page({ params }: PageProps) {
-  console.log('params from page', params.username);
-
+export default async function Page({ params }: Props) {
   const res = await fetch(`http://localhost:3000/api/user/rakeshsangem`, {
     cache: 'no-store',
   });
 
-  console.log('res from page', res);
-
   const user = await res.json();
-
-  console.log('user from page', user?.user?.links);
-
-  // const { name, links, avatar } = user;
-
-  // console.log('user from page', user);
 
   if (!user) {
     return <NotFound />;
   }
 
   return (
-    <div className="p-3 mt-6 sm:p-10 max-w-2xl mx-auto min-h-screen">
-      <div className="w-20 h-20 bg-red-300 rounded-full mx-auto my-2">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={user?.user?.avatar}
-          alt="avatar"
-          className="w-20 h-20 bg-red-300 rounded-full mx-auto my-2"
-        />
-      </div>
-
-      <h1 className="text-3xl font-bold mb-4 text-center">
-        {user?.user?.name}
-      </h1>
-      <p className="text-center">{user?.user?.bio}</p>
-      {user?.user?.links?.length > 0 && (
-        <>
-          {user?.user?.links.map((link: any) => (
-            <a
-              href={link.url}
-              target="__blank"
-              className="bg-white block rounded-md text-center my-4 text-black px-10 sm:py-2.5 py-2"
-              key={link.id}
-            >
-              {link.title}
-            </a>
-          ))}
-        </>
-      )}
-      <br />
-      <footer className="py-4 w-full flex flex-col justify-center">
-        <a
-          href={`https://justlink.vercel.app`}
-          className="opacity-60 w-min mx-auto hover:opacity-100 duration-200 transition ease-out"
-        >
-          <Logo />
-        </a>
-      </footer>
-    </div>
+    <main
+      className={`p-3 sm:p-10 mx-auto min-h-screen w-screen bg-skin-bg ${user?.theme}`}
+    >
+      <section className="max-w-2xl mt-6 mx-auto">
+        <div className="w-20 h-20 bg-red-300 rounded-full mx-auto my-4 outline outline-[0.5px]">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={user?.user?.avatar}
+            alt="avatar"
+            className="w-20 h-20 bg-red-300 rounded-full mx-auto my-2"
+          />
+        </div>
+        <h1 className="text-2xl font-medium text-page-title text-center">
+          {user?.user?.name}
+        </h1>
+        <p className="text-center font-light mt-2 text-white/80">
+          {user?.user?.bio}
+        </p>
+        {user?.user?.links?.length > 0 && (
+          <ul className="space-y-5 my-8">
+            {user?.user?.links.map((link: any) => (
+              <a
+                href={link?.url}
+                target="__blank"
+                className="cursor-pointer block w-full rounded-md bg-skin-btn-bg p-2.5 text-center font-medium text-skin-btn-text outline-offset-1 focus:outline-white/50 transition-all border duration-400 ease-out hover:scale-[1.015] outline-2 active:scale-100 border-skin-btn hover:border-skin-btn"
+                key={link?.id}
+              >
+                {link?.title}
+              </a>
+            ))}
+          </ul>
+        )}
+        <footer className="py-4 w-full flex flex-col justify-center">
+          <a
+            href={`localhost:3000`}
+            className="opacity-60 mx-auto hover:opacity-100 duration-200 transition ease-out"
+          >
+            <Logo />
+          </a>
+        </footer>
+      </section>
+    </main>
   );
 }

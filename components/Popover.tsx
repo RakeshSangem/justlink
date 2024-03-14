fcfbc624c373a3a37b2aa51d94a853382319d0f7
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-03-14 22:34:58 +0530

    feat: add radix base popover component

--- components/Popover.tsx
+++ components/Popover.tsx
@@ -1,52 +1,36 @@
-'use client';
-// Popover.tsx
-import React, { useEffect, useRef, forwardRef, ReactNode } from 'react';
+"use client";
 
-interface PopoverProps {
-  children: ReactNode;
-  isOpen: boolean;
-  onClose: () => void;
-}
+import * as React from "react";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
 
-const PopoverContent = forwardRef<
-  HTMLDivElement,
-  React.HTMLProps<HTMLDivElement>
->((props, ref) => <div ref={ref} {...props} />);
+import { cn } from "@/lib/utils";
 
-PopoverContent.displayName = 'PopoverContent';
+const Popover = PopoverPrimitive.Root;
 
-const Popover: React.FC<PopoverProps> = ({ children, isOpen, onClose }) => {
-  const popoverRef = useRef<HTMLDivElement>(null);
+const PopoverTrigger = PopoverPrimitive.Trigger;
 
-  useEffect(() => {
-    const handleClickOutside = (event: Event) => {
-      if (
-        popoverRef.current &&
-        !popoverRef.current.contains(event.target as Node)
-      ) {
-        onClose();
-      }
-    };
+const PopoverAnchor = PopoverPrimitive.Anchor;
 
-    document.addEventListener('mousedown', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [onClose]);
-
-  return (
-    <>
-      {isOpen && (
-        <div
-          className="animate-slide-up-fade absolute min-w-56 right-0 top-9 z-[999] w-48 bg-[#121215] rounded-md shadow-lg"
-          ref={popoverRef}
-        >
-          <PopoverContent>{children}</PopoverContent>
-        </div>
+const PopoverContent = React.forwardRef<
+  React.ElementRef<typeof PopoverPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
+>(({ className, align = "end", sideOffset = 2, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
+    <PopoverPrimitive.Content
+      ref={ref}
+      align={align}
+      sideOffset={sideOffset}
+      className={cn(
+        "z-50 w-60 rounded-md shadow-lg animate-slide-up-fade",
+        className
       )}
-    </>
-  );
-};
+      {...props}
+    />
+  </PopoverPrimitive.Portal>
+));
 
-export default Popover;
+PopoverContent.displayName = PopoverPrimitive.Content.displayName;
+
+PopoverContent.displayName = "PopoverContent";
+
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor };

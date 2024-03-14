b5a6fef336e2b7f5407bb3b520f6f7adfebae490
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-03-14 22:35:28 +0530

    fix: userInfo popover weird behaviour

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import React, { useState, useRef } from 'react';
-import type { User } from 'next-auth';
-import { logout } from '@/lib/actions/logout';
-import Link from 'next/link';
+import React, { useState, useRef } from "react";
+import type { User } from "next-auth";
+import { logout } from "@/lib/actions/logout";
+import Link from "next/link";
 
-import LogOutIcon from './icons/LogoutIcon';
-import Gear from './icons/Gear';
-import Popover from './Popover';
+import LogOutIcon from "./icons/LogoutIcon";
+import Gear from "./icons/Gear";
+import { Popover, PopoverContent, PopoverTrigger } from "./Popover";
 
 interface UserInfoProps {
   user: User & {
@@ -18,25 +18,20 @@ interface UserInfoProps {
 }
 
 export default function UserInfo({ user }: UserInfoProps) {
-  const [showPopOver, setShowPopOver] = useState(false);
-  const buttonRef = useRef(null);
-
   return (
-    <div className="relative">
-      <button
-        ref={buttonRef}
-        onClick={() => setShowPopOver(!showPopOver)}
-        className="relative flex justify-between border border-white/40 rounded-full hover:bg-white/10"
-      >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={user?.image}
-          alt={user?.name}
-          className="w-8 h-8 rounded-full"
-        />
-      </button>
-      <Popover isOpen={showPopOver} onClose={() => setShowPopOver(false)}>
-        <div className="rounded-xl border-[0.5px] border-[#333333] px-4 py-3 text-white">
+    <Popover>
+      <PopoverTrigger asChild>
+        <button className="relative flex justify-between border border-white/40 rounded-full hover:bg-white/10">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={user?.image}
+            alt={user?.name}
+            className="w-8 h-8 rounded-full"
+          />
+        </button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="rounded-xl border-[0.5px] bg-black border-[#333333] px-4 py-3 text-white">
           <div className="m-2 text-start">
             <p className="font-normal text-white/80 truncate">{user?.name}</p>
             <p className="text-sm font-light text-white/60 truncate">
@@ -56,12 +51,12 @@ export default function UserInfo({ user }: UserInfoProps) {
               onClick={() => logout()}
               className="group flex text-sm w-full items-center gap-x-3 rounded-md px-2 py-1 font-light text-white/70 hover:bg-[#1F1F23] hover:text-white"
             >
-              <LogOutIcon className="group-hover:opacity-100 opacity-80 w-5 h-6" />{' '}
+              <LogOutIcon className="group-hover:opacity-100 opacity-80 w-5 h-6" />{" "}
               Logout
             </button>
           </div>
         </div>
-      </Popover>
-    </div>
+      </PopoverContent>
+    </Popover>
   );
 }
